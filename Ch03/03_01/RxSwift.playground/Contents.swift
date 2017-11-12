8d95e053bd51f57e8b8cbf8bfed1da204cf30fea
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2017-11-12 14:36:28 +0700

    finish section 3 -01

--- Ch03/03_01/RxSwift.playground/Contents.swift
+++ Ch03/03_01/RxSwift.playground/Contents.swift
@@ -2,12 +2,38 @@
 
 import RxSwift
 
-public func example(of description: String, action: () -> Void) {
-    print("\n--- Example of:", description, "---")
-    action()
+example(of: "Just") {
+    let observable = Observable.just("Hello RxSwift")
+    observable.subscribe({ (event) in
+        print(event)
+    })
 }
 
+example(of: "of") {
+    let observable = Observable.of(1,2,3)
+    observable.subscribe({
+        print($0)
+    })
+    
+}
 
+example(of: "from") {
+    let disposeBag = DisposeBag()
+    let subscription = Observable.from([1,2,3]).subscribe(onNext: { (nextValue) in
+        print("onNext: \(nextValue)")
+    }, onError: { (error) in
+        print("onError: \(error)")
+    }, onCompleted: {
+        print("onComplete")
+    }, onDisposed: {
+        print("onDispose")
+    })
+    subscription.disposed(by: disposeBag)
+}
+
+example(of: "error") {
+    
+}
 
 /*:
  Copyright © 2017 Optimac, Inc. All rights reserved.
