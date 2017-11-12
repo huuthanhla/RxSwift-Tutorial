57c6138aad45e8754630893bf7ae917b856e163d
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2017-11-12 15:05:51 +0700

    finish 03-03

--- Ch03/03_03/RxSwift.playground/Contents.swift
+++ Ch03/03_03/RxSwift.playground/Contents.swift
@@ -2,29 +2,37 @@
 
 import RxSwift
 
-example(of: "PublishSubject") {
-    
-    
-}
 
 /*:
- Copyright © 2017 Optimac, Inc. All rights reserved.
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
+ ## PublishSubject
+ Broadcasts new events to all observers as of their time of the subscription.
+ ![](https://raw.githubusercontent.com/kzaher/rxswiftcontent/master/MarbleDiagrams/png/publishsubject.png "PublishSubject")
  */
+example(of: "PublishSubject") {
+    
+    let subject = PublishSubject<String>()
+    let disposeBag = DisposeBag()
+    
+    subject.subscribe {
+        print($0)
+    }
+    .disposed(by: disposeBag)
+    
+    enum MyError: Error {
+        case test
+    }
+    
+    subject.onNext("Haha")
+    //subject.onCompleted()
+    //subject.onError(MyError.test)
+    subject.on(.next("Hello world"))
+    
+    let newSubscription = subject.subscribe(onNext: {
+        print("NewSubscription: \($0)")
+    })
+    
+    subject.onNext("What's up man!!!")
+    newSubscription.dispose()
+    subject.onNext("still there?")
+    
+}
