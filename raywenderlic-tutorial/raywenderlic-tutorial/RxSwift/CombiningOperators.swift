d370d4d026c432994de3030d58820fdf61e68d5c
NgoHongThai <hongthai.ng@samsung.com>
2018-05-02 17:37:46 +0700

    combining operators

--- raywenderlic-tutorial/raywenderlic-tutorial/RxSwift/CombiningOperators.swift
+++ raywenderlic-tutorial/raywenderlic-tutorial/RxSwift/CombiningOperators.swift
@@ -115,6 +115,17 @@ class CombiningOperators {
         }
         
         //MARK: - merge()
+        /*
+         NOTE:
+         - merge() completes after its source sequence completes and all inner sequences have completed.
+         - The order in which the inner sequences complete is irrelevant.
+         - If any of the sequences emit an error, the merge() observable immediately relays the error, then terminates.
+         
+         - Notice that merge() takes a source observable, which itself emits observables sequences of the element type.
+           This means that you could send a lot of sequences for merge() to subscribe to!
+         - To limit the number of sequences subscribed to at once, you can use merge(maxConcurrent:)
+         */
+        
         example(of: "merge()") {
             let left = PublishSubject<String>()
             let right = PublishSubject<String>()
@@ -139,5 +150,224 @@ class CombiningOperators {
             
             disposable.dispose()
         }
+        
+        //MARK: - combineLatest()
+        /*
+         NOTE:
+         - Remember that combineLatest(_:_:resultSelector:) waits for all its observables to emit one element before starting to call your closure. It’s a frequent source of confusion and a good opportunity to use the startWith(_:) operator to provide an initial value for the sequences, which could take time to update.
+         
+         */
+        
+        example(of: "combineLatest") {
+            let left = PublishSubject<String>()
+            let right = PublishSubject<String>()
+            let observable = Observable.combineLatest(left, right, resultSelector: { lastLeft, lastRight in
+                "\(lastLeft) - \(lastRight)"
+            })
+            
+            let disposable = observable.subscribe(onNext: { value in
+                print(value)
+            })
+            print("> Sending a value to Left")
+            left.onNext("Hello,")
+            print("> Sending a value to Right")
+            right.onNext("world")
+            print("> Sending another value to Right")
+            right.onNext("RxSwift")
+            print("> Sending another value to Left")
+            left.onNext("Have a good day,")
+            
+            disposable.dispose()
+            /*
+             --- Example of: combineLatest ---
+             > Sending a value to Left
+             > Sending a value to Right
+             Hello, - world
+             > Sending another value to Right
+             Hello, - RxSwift
+             > Sending another value to Left
+             Have a good day, - RxSwift
+             */
+        }
+        
+        /*
+         There are several variants in the combineLatest family of operators.
+         They take between two and eight observable sequences as parameters. As mentioned above, sequences don’t need to have the same element type.
+         
+         This example demonstrates automatic updates of on-screen values when the user settings change. Think about all the manual updates you’ll remove with such patterns!.
+         */
+        
+        example(of: "combine user choice and value") {
+            let choice : Observable<DateFormatter.Style> = Observable.of(.short, .long)
+            let dates = Observable.of(Date())
+            
+            let observable = Observable.combineLatest(choice, dates) {
+                (format, when) -> String in
+                let formatter = DateFormatter()
+                formatter.dateStyle = format
+                return formatter.string(from: when)
+            }
+            
+            observable.subscribe(onNext: { value in
+                print(value)
+            }).disposed(by: bag)
+            /*
+             --- Example of: combine user choice and value ---
+             5/2/18
+             May 2, 2018
+             */
+        }
+        
+        /*
+         A final variant of the combineLatest family takes a collection of observables and a combining closure, which receives latest values in an array. Since it’s a collection, all observables carry elements of the same type. Although less flexible than the multiple parameter variants, it is seldom-used but still handy to know about.
+         
+         Note: Last but not least, combineLatest completes only when the last of its inner sequences completes. Before that, it keeps sending combined values. If some sequences terminate, it uses the last value emitted to combine with new values from other sequences.
+         */
+        example(of: "combineLatest") {
+            let left = PublishSubject<String>()
+            let right = PublishSubject<String>()
+            
+            let observable = Observable.combineLatest([left, right], { strings  in
+                strings.joined(separator: " ")
+            })
+            
+            let disposable = observable.subscribe(onNext: { value in
+                print(value)
+            })
+            print("> Sending a value to Left")
+            left.onNext("Hello")
+            print("> Sending a value to Right")
+            right.onNext("world")
+            print("> Sending another value to Right")
+            right.onNext("RxSwift")
+            print("> Sending another value to Left")
+            left.onNext("Have a good day")
+            
+            disposable.dispose()
+        }
+        
+        //MARK: - zip()
+        /*
+         The explanation lies in the way zip operators work. They wait until each of the inner observables emits a new value. If one of them completes, zip completes as well. It doesn’t wait until all of the inner observables are done! This is called indexed sequencing, which is a way to walk though sequences in lockstep.
+         */
+        example(of: "zip") {
+            enum Weather {
+                case cloudy
+                case sunny
+            }
+            let left: Observable<Weather> = Observable.of(.sunny, .cloudy, .cloudy, .sunny)
+            let right = Observable.of("Lisbon", "Copenhagen", "London", "Madrid","Vienna")
+            
+            let observable = Observable.zip(left, right, resultSelector: { weather, city in
+                "It's \(weather) in \(city)"
+            })
+            observable.subscribe(onNext: { value in
+                print(value)
+            }).disposed(by: bag)
+            
+            /*
+             --- Example of: zip ---
+             It's sunny in Lisbon
+             It's cloudy in Copenhagen
+             It's cloudy in London
+             It's sunny in Madrid
+             */
+        }
+        
+        //MARK: - withLatestFrom(_:)
+        /*
+         Simple and straightforward! withLatestFrom(_:) is useful in all situations where you want the current (latest)
+         value emitted from an observable, but only when a particular trigger occurs.
+         */
+        
+        example(of: "withLatestFrom") {
+            let button = PublishSubject<Void>()
+            let textField = PublishSubject<String>()
+            let observable = button.withLatestFrom(textField)
+            _ = observable.subscribe(onNext: { value in
+                print(value)
+            })
+            textField.onNext("Par")
+            textField.onNext("Pari")
+            textField.onNext("Paris")
+            button.onNext(())
+            button.onNext(())
+            
+            /*
+             --- Example of: withLatestFrom ---
+             Paris
+             Paris
+             */
+        }
+        
+        //MARK: - sample()
+        /*
+         A close relative to withLatestFrom(_:)
+         
+         */
+        
+        example(of: "sample") {
+            let button = PublishSubject<Void>()
+            let textField = PublishSubject<String>()
+            let observable = textField.sample(button)
+            _ = observable.subscribe(onNext: { value in
+                print(value)
+            })
+            textField.onNext("Par")
+            textField.onNext("Pari")
+            textField.onNext("Paris")
+            button.onNext(())
+            button.onNext(())
+            
+            /*
+             --- Example of: sample ---
+             Paris
+             */
+        }
+        /*
+         NOTE:
+         Notice that "Paris" now prints only once!
+         This is because no new value was emitted by the text field between your two fake button presses.
+         You could have achieved the same behavior by adding a distinctUntilChanged() to the withLatestFrom(_:) observable,
+         but smallest possible operator chains are the Zen of RxTM.
+         
+         Note: Don’t forget that withLatestFrom(_:) takes the data observable as a parameter, while sample(_:) takes the trigger observable as a parameter. This can easily be a source of mistakes — so be careful!
+         */
+        
+        //MARK: - amb(_:) - Switches
+        /*
+         - Think of “amb” as in “ambiguous” (mơ hồ)
+         - The amb(_:) operator subscribes to left and right observables.
+           It waits for any of them to emit an element, then unsubscribes from the other one. After that, it only relays elements from the first active observable. (nhận thằng mà có emit đầu tiên sau đó unsubcrible thằng còn lại)
+           It really does draw its name from the term ambiguous: at first, you don’t know which sequence you’re interested in, and want to decide only when one fires.
+         */
+        
+        example(of: "amb()") {
+            let left = PublishSubject<String>()
+            let right = PublishSubject<String>()
+            let observable = right.amb(left)
+            let disposable = observable.subscribe(onNext: { value in
+                print(value)
+            })
+            // 2
+            right.onNext("Copenhagen")
+            left.onNext("Lisbon")
+            left.onNext("London")
+            left.onNext("Madrid")
+            right.onNext("Vienna")
+            disposable.dispose()
+            
+            /*
+             --- Example of: amb() ---
+             Copenhagen
+             Vienna
+             */
+        }
+        
+        //MARK: - switchLatest()
+        /*
+         
+         */
+        
     }
 }
